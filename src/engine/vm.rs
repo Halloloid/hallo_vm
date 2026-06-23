019a869a62e5983278a03f951d110a5f6b721da7
Halloloid <animation12321567@gmail.com>
2026-06-23 13:33:44 +0530

    Only left Load and Store

--- src/engine/vm.rs
+++ src/engine/vm.rs
@@ -1,10 +1,13 @@
 use std::{fs, path::Path};
 
-use crate::engine::{helpers::{check_file_length, check_version, file_type_check}, isa};
+use crate::engine::{
+    helpers::{check_file_length, check_version, file_type_check},
+    isa::{Op, decode},
+};
 
-pub fn execute(file: &str, trace: bool) {
+pub fn execute(file: &str, trace: bool) -> Result<(), String> {
     let mut stack = Vec::<i64>::new();
-    // let globals: [i64; 256] = [0; 256];
+    // let mut globals: [i64; 256] = [0; 256];
     let mut ip: u8 = 0;
 
     let path = Path::new(file);
@@ -12,73 +15,116 @@ pub fn execute(file: &str, trace: bool) {
 
     let mut data = bytes.iter();
 
+    fn pop_stack(stack: &mut Vec<i64>, ip: u8) -> Result<i64, String> {
+        stack.pop().ok_or_else(|| {
+            format!(
+                "trap at ip={:#06x}: stack underflow (POP on empty stack)",
+                ip
+            )
+        })
+    }
+
     if file_type_check(&mut data)
         && check_version(&mut data)
         && check_file_length(&mut data, bytes.len())
     {
-        while let Some(byte) = data.next(){
+        while let Some(byte) = data.next() {
             let mut v = Vec::<u8>::new();
-            if *byte == 1{
+            if *byte == 1 {
                 let mut c = 8;
                 v.push(*byte);
-                while c!=0 {
-                    if let Some(l) = data.next(){
+                while c != 0 {
+                    if let Some(l) = data.next() {
                         v.push(*l);
                     }
-                    c-=1;
+                    c -= 1;
                 }
             } else {
                 v.push(*byte);
             }
 
-            match isa::decode(&v) {
-                Ok((op,wide)) => {
-                    if trace{
-                        println!("{:#06x}\t{}\t{:?}",ip,format!("{:?}",op).to_uppercase(),stack);
+            match decode(&v) {
+                Ok((op, wide)) => {
+                    if trace {
+                        println!(
+                            "{:#06x}   {:<12} {:?}",
+                            ip,
+                            format!("{:?}", op).to_uppercase(),
+                            stack
+                        );
                     }
                     match op {
-                        isa::Op::Push(n) => {
-                            ip+=wide;
-                            stack.push(n);
-                        },
-                        isa::Op::Pop => todo!(),
-                        isa::Op::Dup => todo!(),
-                        isa::Op::Swap => todo!(),
-                        isa::Op::Add => {
-                            ip+=wide;
-                            let Some(a) = stack.pop() else {
-                                eprintln!("trap at ip={:02x}: stack underflow (POP on empty stack)",ip);
-                                return;
-                            };
+                        Op::Push(n) => stack.push(n),
+                        Op::Pop => _ = pop_stack(&mut stack, ip)?,
+                        Op::Dup => {
+                            let a = pop_stack(&mut stack, ip)?;
 
-                            let Some(b) = stack.pop() else {
-                                eprintln!("trap at ip={:02x}: stack underflow (POP on empty stack)",ip);
-                                return;
-                            };
-
-                            stack.push(b+a);
+                            stack.push(a);
+                            stack.push(a);
                         },
-                        isa::Op::Sub => todo!(),
-                        isa::Op::Mul => todo!(),
-                        isa::Op::Div => todo!(),
-                        isa::Op::Mod => todo!(),
-                        isa::Op::Neg => todo!(),
-                        isa::Op::Load(_) => todo!(),
-                        isa::Op::Store(_) => todo!(),
-                        isa::Op::Print => {
-                            ip+=wide;
-                            let Some(n) = stack.pop() else {
-                                eprintln!("trap at ip={:02x}: stack underflow (POP on empty stack)",ip);
-                                return;
-                            };
+                        Op::Swap => {
+                            let a = pop_stack(&mut stack, ip)?;
+                            let b = pop_stack(&mut stack, ip)?;
+
+                            stack.push(a);
+                            stack.push(b);
+                        },
+                        Op::Add => {
+                            let a = pop_stack(&mut stack, ip)?;
+                            let b = pop_stack(&mut stack, ip)?;
+
+                            stack.push(b + a);
+                        }
+                        Op::Sub => {
+                            let a = pop_stack(&mut stack, ip)?;
+                            let b = pop_stack(&mut stack, ip)?;
+
+                            stack.push(b - a);
+                        },
+                        Op::Mul => {
+                            let a = pop_stack(&mut stack, ip)?;
+                            let b = pop_stack(&mut stack, ip)?;
+
+                            stack.push(b * a);
+                        },
+                        Op::Div => {
+                            let a = pop_stack(&mut stack, ip)?;
+                            let b = pop_stack(&mut stack, ip)?;
+
+                            if a == 0 {
+                                Err(format!("trap at ip={:#06x}: DIV by Zero",ip))?;
+                            }
+                            
+                            stack.push(b / a);
+                        },
+                        Op::Mod => {
+                            let a = pop_stack(&mut stack, ip)?;
+                            let b = pop_stack(&mut stack, ip)?;
+
+                            if a == 0 {
+                                Err(format!("trap at ip={:#06x}: MOD by Zero",ip))?;
+                            }
+                            
+                            stack.push(b % a);
+                        },
+                        Op::Neg => {
+                            let a = pop_stack(&mut stack, ip)?;
+                            stack.push(-a);
+                        },
+                        Op::Load(_) => todo!(),
+                        Op::Store(_) => todo!(),
+                        Op::Print => {
+                            let n = pop_stack(&mut stack, ip)?;
                             println!("{n}");
-                        },
-                        isa::Op::Halt => {},
+                        }
+                        Op::Halt => break,
                     }
-                    
-                },
+                    ip += wide;
+                }
                 Err(_) => todo!(),
             }
         }
     }
+
+    Ok(())
 }
