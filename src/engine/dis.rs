600a56a91616d9a767634ab18f5c0eba3714d70c
Halloloid <animation12321567@gmail.com>
2026-06-23 09:33:32 +0530

    Modified Disassembler

--- src/engine/dis.rs
+++ src/engine/dis.rs
@@ -1,4 +1,7 @@
-use crate::engine::isa::{DecodeError, decode};
+use crate::engine::{
+    helpers::{check_version, file_type_check},
+    isa::{DecodeError, decode},
+};
 use std::{fs, path::Path};
 
 pub fn run(file: &str) {
@@ -6,46 +9,31 @@ pub fn run(file: &str) {
     let bytes = fs::read(path).expect("Unabel to read The file");
     let mut data = bytes.iter();
 
-    let mut type_of_file = Vec::<u8>::new();
-    let mut c = 4;
-    while c!=0 && let Some(i) = data.next(){
-        type_of_file.push(*i);
-        c -= 1;
-    }
-
-    if type_of_file.len() == 4 {
-        if type_of_file[0] == 77
-            && type_of_file[1] == 86
-            && type_of_file[2] == 77
-            && type_of_file[3] == 0
-        {
-            if let Some(version) = data.next() {
-                if *version == 1 {
-                    for _ in 0..4 {data.next();} // skiping the length bytes
-                    while let Some(i) = data.next() {
-                        let mut v = Vec::<u8>::new();
-                        if *i == 1 {
-                            let mut c = 8;
-                            v.push(*i);
-                            while c != 0 {
-                                if let Some(l) = data.next() {
-                                    v.push(*l);
-                                }
-                                c -= 1;
-                            }
-                        } else {
-                            v.push(*i);
+    if file_type_check(&mut data) {
+        if check_version(&mut data) {
+            for _ in 0..4 { data.next(); } // skiping the length bytes
+            while let Some(i) = data.next() {
+                let mut v = Vec::<u8>::new();
+                if *i == 1 {
+                    let mut c = 8;
+                    v.push(*i);
+                    while c != 0 {
+                        if let Some(l) = data.next() {
+                            v.push(*l);
                         }
+                        c -= 1;
+                    }
+                } else {
+                    v.push(*i);
+                }
 
-                        match decode(&v) {
-                            Ok((op, _)) => {
-                                println!("{}", format!("{:?}",op).to_uppercase());
-                            }
-                            Err(DecodeError::Truncated) => todo!(),
-                            Err(DecodeError::UnknownOpcode(c)) => {
-                                println!("Unknnown Code:{:02x}", c)
-                            }
-                        }
+                match decode(&v) {
+                    Ok((op, _)) => {
+                        println!("{}", format!("{:?}", op).to_uppercase());
+                    }
+                    Err(DecodeError::Truncated) => todo!(),
+                    Err(DecodeError::UnknownOpcode(c)) => {
+                        println!("Unknnown Code:{:02x}", c)
                     }
                 }
             }
