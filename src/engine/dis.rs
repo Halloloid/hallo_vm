ed9b777f546b6ce6206fea6577f01d5e78a4c96a
Halloloid <animation12321567@gmail.com>
2026-06-23 10:02:12 +0530

    Remodeled Disassembler

--- src/engine/dis.rs
+++ src/engine/dis.rs
@@ -1,5 +1,5 @@
 use crate::engine::{
-    helpers::{check_version, file_type_check},
+    helpers::{check_file_length, check_version, file_type_check},
     isa::{DecodeError, decode},
 };
 use std::{fs, path::Path};
@@ -9,32 +9,32 @@ pub fn run(file: &str) {
     let bytes = fs::read(path).expect("Unabel to read The file");
     let mut data = bytes.iter();
 
-    if file_type_check(&mut data) {
-        if check_version(&mut data) {
-            for _ in 0..4 { data.next(); } // skiping the length bytes
-            while let Some(i) = data.next() {
-                let mut v = Vec::<u8>::new();
-                if *i == 1 {
-                    let mut c = 8;
-                    v.push(*i);
-                    while c != 0 {
-                        if let Some(l) = data.next() {
-                            v.push(*l);
-                        }
-                        c -= 1;
+    if file_type_check(&mut data)
+        && check_version(&mut data)
+        && check_file_length(&mut data, bytes.len())
+    {
+        while let Some(i) = data.next() {
+            let mut v = Vec::<u8>::new();
+            if *i == 1 {
+                let mut c = 8;
+                v.push(*i);
+                while c != 0 {
+                    if let Some(l) = data.next() {
+                        v.push(*l);
                     }
-                } else {
-                    v.push(*i);
+                    c -= 1;
                 }
+            } else {
+                v.push(*i);
+            }
 
-                match decode(&v) {
-                    Ok((op, _)) => {
-                        println!("{}", format!("{:?}", op).to_uppercase());
-                    }
-                    Err(DecodeError::Truncated) => todo!(),
-                    Err(DecodeError::UnknownOpcode(c)) => {
-                        println!("Unknnown Code:{:02x}", c)
-                    }
+            match decode(&v) {
+                Ok((op, _)) => {
+                    println!("{}", format!("{:?}", op).to_uppercase());
+                }
+                Err(DecodeError::Truncated) => todo!(),
+                Err(DecodeError::UnknownOpcode(c)) => {
+                    println!("Unknnown Code:{:02x}", c)
                 }
             }
         }
